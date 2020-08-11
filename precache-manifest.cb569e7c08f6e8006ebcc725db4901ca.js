ae5c9667e2013c749e2c5c0d06788b205b231b53
Sthefano Carvalho <sthefano@masteradin.com>
2020-08-10 21:10:33 -0300

    Updates

---
+++ precache-manifest.cb569e7c08f6e8006ebcc725db4901ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bcb6e1fceeb0ccac708f79ad5eef691f",
+    "url": "/rich-text-editor/index.html"
+  },
+  {
+    "revision": "682d01be938ce83b1c04",
+    "url": "/rich-text-editor/static/css/main.0cd88060.chunk.css"
+  },
+  {
+    "revision": "e1554d8d121a24acd7b2",
+    "url": "/rich-text-editor/static/js/2.81010acc.chunk.js"
+  },
+  {
+    "revision": "b23a8a042e51dd51bbd0294055a0b702",
+    "url": "/rich-text-editor/static/js/2.81010acc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "682d01be938ce83b1c04",
+    "url": "/rich-text-editor/static/js/main.6e585503.chunk.js"
+  },
+  {
+    "revision": "7e19add10ade50e03d54",
+    "url": "/rich-text-editor/static/js/runtime-main.234a251a.js"
+  }
+]);
